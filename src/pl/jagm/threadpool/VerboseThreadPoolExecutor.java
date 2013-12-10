19cfc5d70bd878c5dccdd3f90bcc391c061bd55f
jagm <jagiela.mateusz@gmail.com>
2013-12-10 22:52:18 +0100

    add logging events for VerboseThreadPoolExecutor

--- src/pl/jagm/threadpool/VerboseThreadPoolExecutor.java
+++ src/pl/jagm/threadpool/VerboseThreadPoolExecutor.java
@@ -9,4 +9,45 @@ public class VerboseThreadPoolExecutor extends ThreadPoolExecutor {
     public VerboseThreadPoolExecutor(int maximumPoolSize) {
         super(1, maximumPoolSize, 5000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(1));
     }
+
+    @Override
+    public void execute(final Runnable command) {
+        logMetrics("In execute - before");
+        super.execute(new Runnable() {
+            @Override
+            public void run() {
+                logMetrics("In Runnable - before");
+                command.run();
+                logMetrics("In Runnable - after");
+            }
+        });
+        logMetrics("In execute - after");
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        logMetrics("After execute");
+        super.afterExecute(r, t);
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        logMetrics("Before execute");
+        super.beforeExecute(t, r);
+    }
+
+    private void logMetrics(String name) {
+        System.out.println(
+                String.format("[%s] [%d/%d] Active: %d, Completed: %d, Task: %d, isShutdown: %s, isTerminated: %s",
+                        name,
+                        getPoolSize(),
+                        getCorePoolSize(),
+                        getActiveCount(),
+                        getCompletedTaskCount(),
+                        getTaskCount(),
+                        isShutdown(),
+                        isTerminated()));
+    }
+
+
 }
