6e9c877d8ad4bc25101250b8a208e1d90d0af783
jagm <jagiela.mateusz@gmail.com>
2013-12-10 23:00:43 +0100

    add MyThread

---
+++ src/pl/jagm/threadpool/MyThread.java
@@ -0,0 +1,24 @@
+package pl.jagm.threadpool;
+
+import java.util.Random;
+
+public class MyThread implements Runnable {
+
+    private static final Random r = new Random();
+    private final int id;
+
+    public MyThread(int i) {
+        id = i;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("MyThread #" + id + " start");
+        try {
+            Thread.sleep(r.nextInt(10) * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("MyThread #" + id + " stop");
+    }
+}
